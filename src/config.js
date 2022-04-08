3b3cb99ca5a06a3f00748f47f5675402aef8560b
V3llens <luquetes38@gmail.com>
2022-04-08 18:57:48 -0300

    update

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-global.SALT_KEY = '';
+global.SALT_KEY = 'C0CA96F45F103A06D9F1936241E13C8E008B95B3EAEDC9C0C00E3E3817F2CD38';
 global.EMAIL_TMPL = 'olá, <strong>{0}</strong>, seja bem vindo à Node Store"';
 
 module.exports = {
